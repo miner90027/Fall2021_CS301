f22d903a1a314c68054d17ce2944c11766bf7142
miner90027 <alekszander.mccormick@outlook.com>
2021-10-30 20:15:48 -0800

    Modified player() to take input that determines what it returns, default value is 0 added print statements to main() that can be used to test this change still working on how to access the player inventory, might add anoter parameter to player()

--- main.cpp
+++ main.cpp
@@ -1,11 +1,15 @@
 #include <iostream>
 
-extern "C" int player();
-extern "C" int room();
+extern "C" long player(long = 0);
+//extern "C" int room();
 
 int main(){
-
-	std::cout << "player() returns: " << player() 
-							<< "\nroom() returns: "<< room() << std::endl;
+	//std::cout << "player() returns: " << player() << "\nroom() returns: "<< room() << std::endl;
+	
+	
+	std::cout << "Player health: " << player(1) << std::endl;
+	
+	//std::cout << "inventory ptr: " << player(2) << std::endl;
+	
 	return 0;
 }
--- player.asm
+++ player.asm
@@ -1,7 +1,30 @@
 section .text
 	global player
 
-; DUMMY CODE	
+; possible actions
+; 	0 = no action (exit)
+; 	1 = access health
+; 	2 = access inventory
 	player:
-		mov rax, 3
-		ret
+		
+		cmp rdi, 1
+		jne next
+		mov rax, [playerPtr]
+		jmp end
+		
+		next:
+		cmp rdi, 2
+		jne end
+		mov rax, [inventoryPtr]
+		jmp end
+		
+		end:
+			ret
+			
+section .data
+playerPtr:
+	dd 100							; offset 0 Health
+	;dd [inventoryPtr]	; offset 8 inventoryPtr
+
+inventoryPtr:
+	dq 0,0,0,0,0
