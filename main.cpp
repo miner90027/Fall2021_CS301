8f7567c07e7b06a108bc087e3a9d536a25af8f1e
miner90027 <alekszander.mccormick@outlook.com>
2021-12-05 21:47:20 -0900

    Created a printInventory function - written in c++ created asm useItem function that takes an int/long and uses the item in that inventory slot currently doesn't function - no idea why it doesn't work the comparison to check for an invalid index works. The issue is comparing the value stored in mem

--- main.cpp
+++ main.cpp
@@ -3,18 +3,13 @@ using std::cout;
 using std::endl;
 
 extern "C" long getHealth(void);
+extern "C" long playerPtr[];
 extern "C" int inventoryPtr[];
 extern "C" int attack(int);
+extern "C" int useItem(int);
 
 
-int main(){
-	
-	cout << "Player health: " << getHealth() << std::endl;
-	
-	for(int i = 0; i< 5; ++i){
-			inventoryPtr[i] = i+1;
-	}
-	
+void printInventory(){
 	std::cout << "Inventory: " ;
 	
 	for(int i = 0; i < 5; ++i){
@@ -23,6 +18,28 @@ int main(){
 	}
 	
 	cout << std::endl;
+}
+
+int main(){
+	
+	cout << "Player health: " << getHealth() << std::endl;
+	
+	for(int i = 0; i< 5; ++i){
+			inventoryPtr[i] = i+1;
+	}
+
+	printInventory();
+	
+	cout << "Please enter the slot number 0 - 4 for the item you wish to use: " << endl;
+	
+	int in;
+	std::cin >> in;
+	
+	
+	cout << "Using item in slot " << in << " returns: " << useItem(in) << endl;
+	cout << "Item in slot" << in << ": " << inventoryPtr[in] << endl;
+	
+	printInventory();
 	
 	cout << "Player attack enemy. Enemy health remaining: " << attack(0) << endl;
 	
--- player.asm
+++ player.asm
@@ -1,7 +1,9 @@
 section .text
 	global getHealth
 	global inventoryPtr
+	global playerPtr
 	global attack
+	global useItem
 	
 	getHealth:
 		mov rax, QWORD[playerPtr +8*0]
@@ -10,6 +12,63 @@ section .text
 	enemyHealth:
 		mov rax, QWORD[enemyPtr +8*0]
 		ret
+	
+	resetEnemy:
+		xor rax, rax
+		mov QWORD[enemyPtr+8*0], 50
+		mov QWORD[enemyPtr+8*1], 1
+		ret
+		
+	useItem:
+		xor rax, rax
+		xor rcx, rcx
+		cmp rdi, 0
+		jl .invalid
+		cmp rdi, 5
+		jge .invalid
+		
+		mov rcx, QWORD[inventoryPtr +4* rdi]
+		;mov rax, rcx
+		;ret
+		
+		cmp rcx, 1
+		je .potion
+		cmp rcx, 2
+		je .sword
+		cmp rcx, 3
+		je .dagger
+		cmp rcx, 4
+		je .bottle
+		ret
+		
+		.potion:
+			mov QWORD[playerPtr +8*0], 100
+			mov QWORD[inventoryPtr +8* rdi], 4
+			mov rax, rcx
+			ret
+			
+		.sword:
+			mov QWORD[playerPtr +8*1], 20
+			mov rax, rcx
+			ret
+			
+		.dagger:
+			mov QWORD[playerPtr +8*1], 5
+			mov rax, rcx
+			ret
+		
+		.bottle:
+			cmp QWORD[playerPtr +8*0], 100
+			jne .used
+			add QWORD[playerPtr +8*0], 10
+				.used:
+			mov QWORD[inventoryPtr +8* rdi], 0
+			mov rax, rcx
+			ret
+			
+		.invalid:
+			mov rax, -1
+			ret
 	
 	attack:
 		cmp rdi, 0
@@ -43,5 +102,9 @@ enemyPtr:
 	dq 1     ; Attack
 
 inventoryPtr:
-	dq 0,0,0,0,0
+	dq 0
+	dq 0
+	dq 0
+	dq 0
+	dq 0
 
